0f9d55de9ba08d9b76bc670380846a5752723c68
Ismael Pintos <Ismael.Pintos@wowza.com>
2026-03-12 16:26:11 -0300

    Merge branch 'feature/ENG-2985-update-publisher-and-playback-examples' into feature/ENG-3387-improve-publish-play-error-handling

--- src/react-example/src/webrtc/startPlay.js
+++ src/react-example/src/webrtc/startPlay.js
@@ -197,21 +197,6 @@ const websocketSendPlayGetOffer = async (playSettings, websocket, peerConnection
   }
 }
 
-const waitForIceGathering = (pc) => {
-  return new Promise((resolve) => {
-    if (pc.iceGatheringState === "complete") {
-      resolve();
-    } else {
-      const checkState = () => {
-        if (pc.iceGatheringState === "complete") {
-          pc.removeEventListener("icegatheringstatechange", checkState);
-          resolve();
-        }
-      };
-      pc.addEventListener("icegatheringstatechange", checkState);
-    }
-  });
-};
 
 // startPlay
 // callbacks:
